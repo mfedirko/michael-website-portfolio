ef916bec352c5fe1bae44814fa65f6353ddbce0c
Michael Fedirko <no@email.com>
2023-12-16 00:42:45 -0600

    fix ALB health checks

--- server-backend/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ server-backend/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -1,12 +1,12 @@
 package com.helloworld.controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
-public class HelloWorldController 
-{
-@RequestMapping("/")
-public String hello() 
-{
-return "<h1> WELCOME! You have reached Michael's Portfolio. </h1>";
-}
+public class HelloWorldController {
+    @RequestMapping("/")
+    public String hello() {
+        return "<h1> WELCOME! You have reached Michael's Portfolio. </h1>";
+    }
 }
