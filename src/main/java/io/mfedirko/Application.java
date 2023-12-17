bb0653c683b48daf022baa3c0f5ed971729ae771
Michael Fedirko <no@email.com>
2023-12-17 16:28:41 -0600

    update Java package to io.mfedirko, renaming classes

--- src/main/java/io/mfedirko/Application.java
+++ src/main/java/io/mfedirko/Application.java
@@ -1,11 +1,11 @@
-package com.helloworld;
+package io.mfedirko;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
--- src/main/java/io/mfedirko/controller/GreetingController.java
+++ src/main/java/io/mfedirko/controller/GreetingController.java
@@ -1,4 +1,4 @@
-package com.helloworld.controller;
+package io.mfedirko.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
